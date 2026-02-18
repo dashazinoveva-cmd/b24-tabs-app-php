727f1aef305c2e121792ba287d1cea4243ad788a
Dasha <d.zinovieva@consult-info.ru>
2026-02-18 12:45:29 +0300

    fix debug

--- src/config/app.php
+++ src/config/app.php
@@ -1,13 +1,9 @@
 <?php
 
 return [
-    // БД (пока SQLite файл)
     'db_path' => __DIR__ . '/../../storage/app.db',
-
-    // Логи (для отладки install)
     'log_path' => __DIR__ . '/../../storage/app.log',
 
-    // Эти значения потом даст кабинет разработчика Bitrix24
-    'b24_client_id' => getenv('B24_CLIENT_ID') ?: '',
-    'b24_client_secret' => getenv('B24_CLIENT_SECRET') ?: '',
+    'b24_client_id' => 'ТВОЙ_CLIENT_ID_ИЗ_КАБИНЕТА',
+    'b24_client_secret' => 'ТВОЙ_CLIENT_SECRET_ИЗ_КАБИНЕТА',
 ];
